9470d027b7806ae2506ad7fa8f52a099ebf33ea2
Jéssica Espíndola <jessiicav.1@gmail.com>
2024-06-20 23:03:32 -0300

    upload front

--- assets/js/Home.js
+++ assets/js/Home.js
@@ -1,13 +1,9 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet, TouchableOpacity 
-} from "react-native";
-
-
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import NomeShareHeart from "../img/shareheart-logotipo-white.png";
 
 const Home = ({ navigateTo }) => {
+  
   const handleNavigation = () => {
     if (navigateTo) {
       navigateTo("Cadastro");
@@ -16,8 +12,17 @@ const Home = ({ navigateTo }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bem-vindo ao ShareHeart!</Text>
-      
+      <Image source={NomeShareHeart} style={styles.NomeShareHeart}></Image>
+      <Text style={styles.text}>Doar pode salvar uma vida!</Text>
+      <Text style={styles.text}>Que tal começar hoje?</Text>
+      <Text style={styles.text}>Vem com a gente!</Text>
+
+      <TouchableOpacity
+        style={styles.menuItem}
+        onPress={() => handleNavigation("Cadastro")}
+      >
+        <Text style={styles.menuItemText}>Cadastre-se agora</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -29,10 +34,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     // backgroundColor: "#EABFA7",
   },
+  NomeShareHeart: {
+    width: 200,
+    resizeMode: 'contain',
+  },
   text: {
     fontSize: 24,
     color: '#fff',
     marginBottom: 20,
+  },
+  ContainerBotoes: {
+    
   },
   button: {
     backgroundColor: "#ffffff",
@@ -41,6 +53,18 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   buttonText: {
+    fontSize: 16,
+    color: "#5BCEAD",
+    fontWeight: 'bold',
+  },
+  menuItem: {
+    backgroundColor: "#ffffff",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  menuItemText: {
     fontSize: 16,
     color: "#5BCEAD",
     fontWeight: 'bold',
